4725d269f91fa54fac305b775f8617545f03e77a
Nick Adams <nick.pappas.adams@gmail.com>
2023-11-21 12:55:09 -0800

    custom types and enums

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,35 @@
 "use strict";
+// compile ts to js file command: tsc
+// primitive types
 let myName = "Nick";
-console.log(`Name: ${myName}`);
+let isOld = true;
 let myAge = 42;
+console.log(`Name: ${myName}, is old? ${isOld}`);
+// any type
+let x = "Hi there!";
+x = 45;
+// arrays
+let nums = [1, 3, 5, 6];
+let jobs = ["dealer", "trash man", "poker dealer"];
+// tuple
+let person = [42, "Nick", true];
+// tuple array
+let employee;
+employee = [
+    [1, "Nick"],
+    [2, "jack"],
+    [3, "john"]
+];
+// unions
+let id = 22;
+id = "12";
+// enums
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+;
+console.log(Direction1.Up);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,39 @@
+// compile ts to js file command: tsc
+
+// primitive types
 let myName: string = "Nick";
-
-console.log(`Name: ${myName}`);
-
+let isOld: boolean = true;
 let myAge: number = 42;
+console.log(`Name: ${myName}, is old? ${isOld}`);
 
+// any type
+let x: any = "Hi there!";
+x = 45;
+
+// arrays
+let nums: number[] = [1,3,5,6];
+let jobs: string[] = ["dealer", "trash man", "poker dealer"];
+
+// tuple
+let person: [number, string, boolean] = [42, "Nick", true];
+// tuple array
+let employee: [number, string][];
+employee = [
+    [1, "Nick"],
+    [2, "jack"],
+    [3, "john"]
+]
+
+// unions
+let id: string | number = 22;
+id = "12";
+
+// enums
+enum Direction1 {
+    Up = 1,
+    Down = 2,
+    Left = 3,
+    Right = 4
+};
+
+console.log(Direction1.Up);

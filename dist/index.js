02e22a014b1934a95d1729e89491446dff4e4296
Nick Adams <nick.pappas.adams@gmail.com>
2023-11-21 12:42:33 -0800

    added html file for web testing

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,4 @@
 "use strict";
 let myName = "Nick";
+console.log(`Name: ${myName}`);
+let myAge = 42;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 let myName: string = "Nick";
 
+console.log(`Name: ${myName}`);
+
+let myAge: number = 42;
+
